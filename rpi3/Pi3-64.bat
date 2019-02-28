45e17638b38161e0c76f2e3aaa78890274eb6810
Tony Pacheco <tonypacheco.cillis@gmail.com>
2019-02-28 10:18:47 -0800

    Prepared repo for shared working

--- rpi3/Pi3-64.bat
+++ rpi3/Pi3-64.bat
@@ -8,22 +8,13 @@ REM
 @REM COMPILER COMMAND LINE
 @echo off
 set "bindir=C:\Program Files (x86)\GNU ARM AARCH64\bin\"
-set "cpuflags=-O3 -march=armv8-a+simd -mtune=cortex-a53 -mstrict-align -fno-tree-loop-vectorize -fno-tree-slp-vectorize"
+set "cpuflags=-Wall -O3 -march=armv8-a+simd -mtune=cortex-a53 -mstrict-align -fno-tree-loop-vectorize -fno-tree-slp-vectorize"
 set "asmflags=-nostdlib -nostartfiles -ffreestanding -fno-asynchronous-unwind-tables -fomit-frame-pointer -Wa,-a>output/list_rp3.txt"
 set "linkerflags=-Wl,-gc-sections -Wl,--build-id=none -Wl,-Bdynamic -Wl,-Map,output/kernel_rp3.map"
 set "outflags=-o output/kernel_rp3.elf"
 set "libflags=-lc -lm -lgcc"
 @echo on
-"%bindir%aarch64-elf-gcc.exe" %cpuflags% %asmflags% %linkerflags% -Wl,-T,linker/rpi64.ld ^
-  src/main.c ^
-  src/boot/SmartStart64.S ^
-  src/boot/rpi-SmartStart.c ^
-  src/drivers/stdio/emb-stdio.c ^
-  src/drivers/sdcard/SDCard.c ^
-  src/hal/fonts.c ^
-  src/hal/hal_io.c ^
-  %outflags% %libflags%
-
+"%bindir%aarch64-elf-gcc.exe" %cpuflags% %asmflags% %linkerflags% -Wl,-T,linker/rpi64.ld src/main.c src/boot/SmartStart64.S src/boot/rpi-SmartStart.c src/drivers/stdio/emb-stdio.c src/drivers/sdcard/SDCard.c %outflags% %libflags%
 @echo off
 if %errorlevel% EQU 1 (goto build_fail)
 
